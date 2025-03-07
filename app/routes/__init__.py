07be13f58ac5459f0ea5216a1868083bbd701336
santijulian16 <santijulian16@gmail.com>
2025-03-06 21:26:52 -0500

    Se agrego la ruta de usuarios

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,11 +3,15 @@ from flask import Blueprint
 from app.handlers.index import IndexHandler
 from app.handlers.vehicles import VehiclesHandler
 from app.handlers.part_types import PartTypesHandler
+from app.handlers.usuarios import UserHandler;
+from app.handlers.usuarios import UserIDHandler;
 
 
 ROUTES = [
   (IndexHandler, '/'),
   (VehiclesHandler, '/vehicles'),
+  (UserHandler, '/usuarios'),
+  (UserIDHandler, '/usuarios/<int:user_id>'),
   (PartTypesHandler, '/part_types')
 ]
 
