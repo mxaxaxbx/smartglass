9cc7884c9b6b101dd40088d650f42ca33788c40f
santijulian16 <santijulian16@gmail.com>
2025-03-06 21:26:33 -0500

    Version incial

--- app/models/user.py
+++ app/models/user.py
@@ -2,10 +2,38 @@ from app import db
 
 class User(db.Model):
 
-  __tablename__ = 'users'
+  __tablename__ = 'usuarios'
+  __table_args__ = {'schema': 'Smart_Glass'}  # Esquema específico
 
-  id = db.Column(db.Integer, primary_key=True)
-  email = db.Column(db.String(120), unique=True, nullable=False)
-  names = db.Column(db.String(120), nullable=False)
-  surnames = db.Column(db.String(120), nullable=False)
-  last_login = db.Column(db.Integer, nullable=False)
+  userid = db.Column(db.Integer, primary_key=True)
+  primer_nombre = db.Column(db.String(120), nullable=False)
+  segundo_nombre = db.Column(db.String(120), nullable=False)
+  primer_apellido = db.Column(db.String(120), nullable=False)
+  segundo_apellido = db.Column(db.String(120), nullable=False)
+  active = db.Column(db.Boolean, nullable=False)
+  superuser = db.Column(db.Boolean, nullable=False)
+  created = db.Column(db.Integer, nullable=False)
+  update = db.Column(db.Integer, nullable=False)
+  lastlogin = db.Column(db.Integer, nullable=False)
+  email = db.Column(db.String(120), nullable=False)
+  password = db.Column(db.String(120), nullable=False)
+
+  def serialize(self):
+    return {
+      'userid': str(self.userid),
+      'primer_nombre': self.primer_nombre,
+      'segundo_nombre': self.segundo_nombre,
+      'primer_apellido': self.primer_apellido,
+      'segundo_apellido': self.segundo_apellido,
+      'active': self.active,
+      'superuser': self.superuser,
+      'created': self.created,
+      'update': self.update,
+      'lastlogin': self.lastlogin,
+      'email': self.email,
+      'password': self.password
+    }
+  
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
