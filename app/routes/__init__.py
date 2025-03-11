fad26c30a3a8e11cb4f31d0c1f6986aa84191389
santijulian16 <santijulian16@gmail.com>
2025-03-10 23:06:40 -0500

    add user login route to application

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -5,13 +5,15 @@ from app.handlers.vehicles import VehiclesHandler
 from app.handlers.part_types import PartTypesHandler
 from app.handlers.usuarios import UserHandler;
 from app.handlers.usuarios import UserIDHandler;
+from app.handlers.usuarios import UserLogin;
 
 
 ROUTES = [
   (IndexHandler, '/'),
-  (VehiclesHandler, '/vehicles'),
+  (UserLogin, '/login'),
   (UserHandler, '/usuarios'),
   (UserIDHandler, '/usuarios/<int:user_id>'),
+  (VehiclesHandler, '/vehicles'),
   (PartTypesHandler, '/part_types')
 ]
 
