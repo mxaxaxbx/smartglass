961c67c7c08fa6f892430eeea1282f8dc1f4c1d9
santijulian16 <santijulian16@gmail.com>
2025-03-10 23:06:22 -0500

    add JWT authentication setup and configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from flask_restful import Api
-from flask_sqlalchemy import SQLAlchemy 
+from flask_sqlalchemy import SQLAlchemy
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required
 
 from os import getenv, environ
 
@@ -14,6 +15,15 @@ app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
 db = SQLAlchemy(app)
 
+
+secret_key = getenv('SECRET_KEY')
+jwt_secret_key = getenv('JWT_SECRET_KEY')
+app.config['SECRET_KEY'] = secret_key
+app.config["JWT_SECRET_KEY"] = jwt_secret_key
+app.config['JWT_TOKEN_LOCATION'] = ['headers']
+
+jwt = JWTManager(app)
+
 from app.routes import ROUTES, api_bp
 
 api = Api(api_bp)
@@ -22,4 +32,6 @@ for resource, route in ROUTES:
   api.add_resource(resource, route)
 
 app.register_blueprint(api_bp, url_prefix='/api')
+
+
 
