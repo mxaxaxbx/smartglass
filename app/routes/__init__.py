67da1c3622abfc4430d51bdb1bfe3e9cc66538dc
santijulian16 <santijulian16@gmail.com>
2025-04-10 14:45:22 -0500

    Refactor routes to include RolHandler and clean up commented imports

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,12 +1,13 @@
 from flask import Blueprint
 
 from app.handlers.index import IndexHandler
-from app.handlers.vehicles import VehiclesHandler
-from app.handlers.part_types import PartTypesHandler
 from app.handlers.usuarios import UserHandler;
 from app.handlers.usuarios import UserIDHandler;
 from app.handlers.usuarios import UserLogin;
 from app.handlers.usuarios import CurrentUserHandler;
+from app.handlers.roles import RolHandler;
+#from app.handlers.vehicles import VehiclesHandler
+#from app.handlers.part_types import PartTypesHandler
 
 
 ROUTES = [
@@ -14,9 +15,10 @@ ROUTES = [
   (UserLogin, '/login'),
   (UserHandler, '/usuarios'),
   (UserIDHandler, '/usuarios/<int:user_id>'),
-  (CurrentUserHandler, '/usuarios/current'),
-  (VehiclesHandler, '/vehicles'),
-  (PartTypesHandler, '/part_types')
+  (CurrentUserHandler, '/usuarios/current'),  
+  (RolHandler, '/roles')
+  #(VehiclesHandler, '/vehicles'),
+  #(PartTypesHandler, '/part_types')
 ]
 
 api_bp = Blueprint('api', __name__)
