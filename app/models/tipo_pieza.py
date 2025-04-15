02008c25cb3d583a7eb102aaeabcf4b5423b4431
santijulian16 <santijulian16@gmail.com>
2025-04-15 10:11:44 -0500

    Refactor TipoPieza model to update table name, add schema, and include new fields for creation and update timestamps

--- app/models/tipo_pieza.py
+++ app/models/tipo_pieza.py
@@ -2,10 +2,13 @@ from app import db
 
 class TipoPieza(db.Model):
 
-  __tablename__ = 'part_types'
+  __tablename__ = 'tipos_piezas'
+  __table_args__ = {'schema': 'smartglass'}  # Esquema específico
 
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(120), nullable=False)
+  idtpopieza = db.Column(db.Integer, primary_key=True)
+  nombre = db.Column(db.String(120), nullable=False)
+  created = db.Column(db.Integer, nullable=False)
+  update = db.Column(db.Integer, nullable=False)
 
   def save(self):
     db.session.add(self)
@@ -13,6 +16,11 @@ class TipoPieza(db.Model):
 
   def serialize(self):
     return {
-      'id': str(self.id),
-      'name': self.name
+      'idtpopieza': str(self.idtpopieza),
+      'nombre': self.nombre,
+      'update': self.update,
+      'created': self.created
     }
+  
+  def put(self):
+    db.session.commit()
