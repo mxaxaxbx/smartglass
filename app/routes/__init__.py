2930e15f4f3e677fd343a2ef3f233336ca3a9daf
santijulian16 <santijulian16@gmail.com>
2025-04-15 09:38:22 -0500

    Update route handlers to use UsuarioHandler and VehiculoHandler for user and vehicle management

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,25 +1,25 @@
 from flask import Blueprint
 
 from app.handlers.index import IndexHandler
-from app.handlers.usuarios import UserHandler;
+from app.handlers.usuarios import UsuarioHandler;
 from app.handlers.usuarios import UserIDHandler;
 from app.handlers.usuarios import UserLogin;
 from app.handlers.usuarios import CurrentUserHandler;
 from app.handlers.roles import RolHandler;
 from app.handlers.ordenes import OrdenHandler;
-#from app.handlers.vehicles import VehiclesHandler
+from app.handlers.vehiculos import VehiculoHandler
 #from app.handlers.part_types import PartTypesHandler
 
 
 ROUTES = [
   (IndexHandler, '/'),
   (UserLogin, '/login'),
-  (UserHandler, '/usuarios'),
+  (UsuarioHandler, '/usuarios'),
   (UserIDHandler, '/usuarios/<int:user_id>'),
   (CurrentUserHandler, '/usuarios/current'),  
   (RolHandler, '/roles'),
-  (OrdenHandler, '/ordenes')
-  #(VehiclesHandler, '/vehicles'),
+  (OrdenHandler, '/ordenes'),
+  (VehiculoHandler, '/vehiculos')
   #(PartTypesHandler, '/part_types')
 ]
 
