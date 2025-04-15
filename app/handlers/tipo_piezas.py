e2c8cd9f7c516a29abcf8121130fb63318f6f783
santijulian16 <santijulian16@gmail.com>
2025-04-15 10:11:35 -0500

    Add JWT authentication and implement GET, POST, and PUT methods in TipoPiezasHandler for managing part types

--- app/handlers/tipo_piezas.py
+++ app/handlers/tipo_piezas.py
@@ -1,29 +1,58 @@
 from flask_restful import Resource, request
 
 from app.models.tipo_pieza import TipoPieza
+from time import time
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 class TipoPiezasHandler(Resource):
 
+  @jwt_required()
   def get(self):
-    ptypes = TipoPieza.query.all()
-    ptypes = [ptype.serialize() for ptype in ptypes]
-    return ptypes
+    tipoPiezas = TipoPieza.query.all()
+    tipoPieza_js = [lo_tipoPieza.serialize() for lo_tipoPieza in tipoPiezas]
+    return tipoPieza_js
 
-  """
-  Request
-  [
-    { 'name': 'Panoramico' },
-    { 'name': 'Trasero' },
-    { 'name': 'Delantero' },
-    { 'name': 'Lateral' },
-    { 'name': 'Central' },
-  ]
-  """
+  @jwt_required()
   def post(self):
     data = request.get_json()
+    created = int(time())
 
-    for item in data:
-      ptype = TipoPieza(name=item['name'])
-      ptype.save()
+    #for item in data:
+    lo_tipoPieza = TipoPieza(nombre=data['nombre'], created=created, update=None)
+    lo_tipoPieza.save()
 
-    return { 'message': 'part types created successfully' }
+    return { 'message': 'Tipo de Pieza creado correctamente' }
+  
+  @jwt_required()
+  def put(self):
+    data = request.json  # Datos enviados en la solicitud
+    msg = ""
+
+    if "idtpopieza" in data:
+      lo_tipoPieza = TipoPieza.query.get(data["idtpopieza"])
+      if(lo_tipoPieza is not None):
+            # Actualizar solo si el dato es proporcionado
+        if "nombre" in data:
+            lo_tipoPieza.nombre = data["nombre"]
+
+        lo_tipoPieza.update = int(time())
+        
+        lo_tipoPieza.put()
+        msg = {'message': 'Tipo de Pieza actualizado correctamente.'}
+      else:
+        msg = {'error': 'No existe la Tipo de Pieza a actualizar.'}, 404
+    else:
+      msg = {'error': 'No existe atributo "idtpopieza" en payload enviado.'}, 404
+
+    return msg
+  
+"""class UserIDHandler(Resource):
+  @jwt_required()
+  def get(self, user_id=None):
+    users2 = User.query.get(user_id)
+    if(users2 is not None):
+      users2 = users2.serialize()
+    else:
+      users2 = {'error': 'No existe el usuario prueba'}, 404
+    return users2
+"""
