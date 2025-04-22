a0f4fc01293bf386601935e51cee557d8dc192b6
santijulian16 <santijulian16@gmail.com>
2025-04-22 10:37:28 -0500

    Refactor imports in routes to consolidate UsuarioHandler imports and maintain organization

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,10 +1,7 @@
 from flask import Blueprint
 
 from app.handlers.index import IndexHandler
-from app.handlers.usuarios import UsuarioHandler;
-from app.handlers.usuarios import UserIDHandler;
-from app.handlers.usuarios import UserLogin;
-from app.handlers.usuarios import CurrentUserHandler;
+from app.handlers.usuarios import UsuarioHandler, UserIDHandler, UserLogin, CurrentUserHandler;
 from app.handlers.roles import RolHandler;
 from app.handlers.ordenes import OrdenHandler;
 from app.handlers.vehiculos import VehiculoHandler;
@@ -12,6 +9,11 @@ from app.handlers.tipo_piezas import TipoPiezasHandler;
 from app.handlers.tipo_solicitudes import TipoSolicitudesHandler;
 from app.handlers.rutasproduccion import RutasProduccionHandler;
 from app.handlers.piezas import PiezasHandler;
+from app.handlers.areas import AreasHandler;
+from app.handlers.etapas import EtapasHandler;
+from app.handlers.rprdcion_etapas import RPrdcion_EtapasHandler;
+from app.handlers.procesos import ProcesoHandler, ProcesoIDHandler;
+from app.handlers.historial_procesos import HistorialProcesoHandler, HistorialProcesoIDHandler;
 
 
 ROUTES = [
@@ -27,6 +29,14 @@ ROUTES = [
   (TipoSolicitudesHandler, '/tipossolicitudes'),
   (RutasProduccionHandler, '/rutasproduccion'),
   (PiezasHandler, '/piezas'),
+  (AreasHandler, '/areas'),
+  (EtapasHandler, '/etapas'),
+  (RPrdcion_EtapasHandler, '/rprdcion_etapas'),
+  (ProcesoHandler, '/procesos'),
+  (ProcesoIDHandler, '/procesos/<int:proceso_id>'),
+  (HistorialProcesoHandler, '/historialprocesos'),
+  (HistorialProcesoIDHandler, '/historialprocesos/<h_proceso_id>'),
+
 ]
 
 api_bp = Blueprint('api', __name__)
