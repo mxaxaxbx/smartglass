ba53746f20e70a07a6ad65e47fd88903552aa1c9
santijulian16 <santijulian16@gmail.com>
2025-04-22 10:37:05 -0500

    Add Etapa model for managing stage data with save, serialize, and put methods

---
+++ app/models/etapa.py
@@ -0,0 +1,30 @@
+from app import db
+
+class Etapa(db.Model):
+
+  __tablename__ = 'etapas'
+  __table_args__ = {'schema': 'smartglass'}  # Esquema específico
+
+  idetapa = db.Column(db.Integer, primary_key=True)
+  areaid = db.Column(db.Integer, nullable=False)
+  nombre = db.Column(db.String(120), nullable=False)
+  etapafinal = db.Column(db.Boolean, nullable=False)
+  created = db.Column(db.Integer, nullable=False)
+  update = db.Column(db.Integer, nullable=False)
+
+  def save(self):
+    db.session.add(self)
+    db.session.commit()
+
+  def serialize(self):
+    return {
+      'idetapa': str(self.idetapa),
+      'areaid': str(self.areaid),
+      'nombre': self.nombre,
+      'etapafinal': self.etapafinal,
+      'update': self.update,
+      'created': self.created
+    }
+  
+  def put(self):
+    db.session.commit()
