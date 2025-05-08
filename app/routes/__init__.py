b1edc415439b5553dcddb53c063e9817f43a0e57
santijulian16 <santijulian16@gmail.com>
2025-05-08 09:35:11 -0500

    Refactor route imports for improved organization and clarity

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -2,16 +2,16 @@ from flask import Blueprint
 
 from app.handlers.index import IndexHandler
 from app.handlers.usuarios import UsuarioHandler, UserIDHandler, UserLogin, CurrentUserHandler;
-from app.handlers.roles import RolHandler;
+from app.handlers.roles import RolHandler, RolIDHandler;
 from app.handlers.ordenes import OrdenHandler;
 from app.handlers.vehiculos import VehiculoHandler, VehiculoIDHandler;
-from app.handlers.tipo_piezas import TipoPiezasHandler;
+from app.handlers.tipo_piezas import TipoPiezasHandler, TpoPiezaIDHandler;
 from app.handlers.tipo_solicitudes import TipoSolicitudesHandler, TpoSolIDHandler;
-from app.handlers.rutasproduccion import RutasProduccionHandler;
+from app.handlers.rutasproduccion import RutasProduccionHandler, RtaProdIDHandler;
 from app.handlers.piezas import PiezasHandler;
-from app.handlers.areas import AreasHandler;
-from app.handlers.etapas import EtapasHandler;
-from app.handlers.rprdcion_etapas import RPrdcion_EtapasHandler;
+from app.handlers.areas import AreasHandler, AreaIDHandler;
+from app.handlers.etapas import EtapasHandler, EtapaIDHandler;
+from app.handlers.rprdcion_etapas import RPrdcion_EtapasHandler, RtaPrdEtpaIDHandler;
 from app.handlers.procesos import ProcesoHandler, ProcesoIDHandler;
 from app.handlers.historial_procesos import HistorialProcesoHandler, HistorialProcesoIDHandler;
 
@@ -23,17 +23,23 @@ ROUTES = [
   (UserIDHandler, '/usuarios/<int:user_id>'),
   (CurrentUserHandler, '/usuarios/current'),  
   (RolHandler, '/roles'),
+  (RolIDHandler, '/roles/<int:idrol>'),
   (OrdenHandler, '/ordenes'),
   (VehiculoHandler, '/vehiculos'),
   (VehiculoIDHandler, '/vehiculos/<int:idvehiculo>'),
   (TipoPiezasHandler, '/tipospiezas'),
+  (TpoPiezaIDHandler, '/tipospiezas/<int:idtpopieza>'),
   (TipoSolicitudesHandler, '/tipossolicitudes'),
   (TpoSolIDHandler, '/tipossolicitudes/<int:idtposol>'),
   (RutasProduccionHandler, '/rutasproduccion'),
+  (RtaProdIDHandler, '/rutasproduccion/<int:idrutapracion>'),
   (PiezasHandler, '/piezas'),
   (AreasHandler, '/areas'),
+  (AreaIDHandler, '/areas/<int:idarea>'),
   (EtapasHandler, '/etapas'),
+  (EtapaIDHandler, '/etapas/<int:idetapa>'),
   (RPrdcion_EtapasHandler, '/rprdcion_etapas'),
+  (RtaPrdEtpaIDHandler, '/rprdcion_etapas/<int:id_rprdcion_etapa>'),
   (ProcesoHandler, '/procesos'),
   (ProcesoIDHandler, '/procesos/<int:proceso_id>'),
   (HistorialProcesoHandler, '/historialprocesos'),
