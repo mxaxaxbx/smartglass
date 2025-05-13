6a04decb80c8b6c9d4ce14d8fbb1edd5e859f26c
santijulian16 <santijulian16@gmail.com>
2025-05-13 12:51:41 -0500

    Add OrderIDHandler to routes for Orden model

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -3,7 +3,7 @@ from flask import Blueprint
 from app.handlers.index import IndexHandler
 from app.handlers.usuarios import UsuarioHandler, UserIDHandler, UserLogin, CurrentUserHandler;
 from app.handlers.roles import RolHandler, RolIDHandler;
-from app.handlers.ordenes import OrdenHandler;
+from app.handlers.ordenes import OrdenHandler, OrderIDHandler;
 from app.handlers.vehiculos import VehiculoHandler, VehiculoIDHandler;
 from app.handlers.tipo_piezas import TipoPiezasHandler, TpoPiezaIDHandler;
 from app.handlers.tipo_solicitudes import TipoSolicitudesHandler, TpoSolIDHandler;
@@ -25,6 +25,7 @@ ROUTES = [
   (RolHandler, '/roles'),
   (RolIDHandler, '/roles/<int:idrol>'),
   (OrdenHandler, '/ordenes'),
+  (OrderIDHandler, '/ordenes/<int:ordenid>'),
   (VehiculoHandler, '/vehiculos'),
   (VehiculoIDHandler, '/vehiculos/<int:idvehiculo>'),
   (TipoPiezasHandler, '/tipospiezas'),
