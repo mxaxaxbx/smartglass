be7c0b91848d6fe6aeed53a5cebe48ec7fca249b
santijulian16 <santijulian16@gmail.com>
2025-05-13 14:49:22 -0500

    Enhance Orden creation and update methods to enforce estado validation and return ordenid in response

--- app/handlers/ordenes.py
+++ app/handlers/ordenes.py
@@ -22,7 +22,8 @@ class OrdenHandler(Resource):
     lo_orden = Orden(fechapedido=data['fechapedido'], cliente=data['cliente'], torrecliente=data['torrecliente'], estado=estado, created=created, update=None)
     lo_orden.save()
 
-    return { 'message': 'Orden Creado correctamente' }
+    return { 'message': 'Orden Creado correctamente',
+             'ordenid': str(lo_orden.ordenid)  }
     
   @jwt_required()
   def put(self):
@@ -32,20 +33,23 @@ class OrdenHandler(Resource):
     if "ordenid" in data:
       lo_orden = Orden.query.get(data["ordenid"])
       if(lo_orden is not None):
+        if lo_orden.estado == 'REGISTRADO' or lo_orden.estado == 'RECHAZADO'  or lo_orden.estado == '' or lo_orden.estado is None:
             # Actualizar solo si el dato es proporcionado
-        if "fechapedido" in data:
-            lo_orden.fechapedido = data["fechapedido"]
-        if "cliente" in data:
-            lo_orden.cliente = data["cliente"]
-        if "torrecliente" in data:
-            lo_orden.torrecliente = data["torrecliente"]
-        if "estado" in data:
-            lo_orden.estado = data["estado"]
+          if "fechapedido" in data:
+              lo_orden.fechapedido = data["fechapedido"]
+          if "cliente" in data:
+              lo_orden.cliente = data["cliente"]
+          if "torrecliente" in data:
+              lo_orden.torrecliente = data["torrecliente"]
+          
+          lo_orden.estado = 'REGISTRADO'
 
-        lo_orden.update = int(time())
-        
-        lo_orden.put()
-        msg = {'message': 'Orden actualizado correctamente.'}
+          lo_orden.update = int(time())
+          
+          lo_orden.put()
+          msg = {'message': 'Orden actualizado correctamente.'}
+        else:
+          msg = {'error': 'No se puede actualizar la orden, ya que no está en estado REGISTRADO.'}, 403
       else:
         msg = {'error': 'No existe la orden a actualizar.'}, 404
     else:
@@ -62,3 +66,27 @@ class OrderIDHandler(Resource):
     else:
       order2 = {'error': 'No existe el usuario prueba'}, 404
     return order2
+
+class OrderIDEstadoHandler(Resource):
+  @jwt_required()
+  def put(self):
+    data = request.json  # Datos enviados en la solicitud
+    msg = ""
+
+    if "ordenid" in data:
+      lo_orden = Orden.query.get(data["ordenid"])
+      if(lo_orden is not None):
+        # Actualizar solo si el dato es proporcionado
+        if "estado" in data:
+          lo_orden.estado = data["estado"]
+
+        lo_orden.update = int(time())
+          
+        lo_orden.put()
+        msg = {'message': 'Estado de Orden actualizado correctamente.'}
+      else:
+        msg = {'error': 'No existe la orden a actualizar.'}, 404
+    else:
+      msg = {'error': 'No existe atributo "ordenid" en payload enviado.'}, 404
+
+    return msg
