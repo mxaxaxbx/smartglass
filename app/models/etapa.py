31a31f05363477aac14096501c27534d09105a78
santijulian16 <santijulian16@gmail.com>
2025-05-15 10:29:22 -0500

    Add foreign key constraint for areaid in Etapa model and update relationship for hsProcesos

--- app/models/etapa.py
+++ app/models/etapa.py
@@ -6,11 +6,12 @@ class Etapa(db.Model):
   __table_args__ = {'schema': 'smartglass'}  # Esquema específico
 
   idetapa = db.Column(db.Integer, primary_key=True)
-  areaid = db.Column(db.Integer, nullable=False)
+  areaid = db.Column(db.Integer, db.ForeignKey('smartglass.areas.idarea'), nullable=False)
   nombre = db.Column(db.String(120), nullable=False)
   etapafinal = db.Column(db.Boolean, nullable=False)
   created = db.Column(db.Integer, nullable=False)
   update = db.Column(db.Integer, nullable=False)
+  hsProcesos = db.relationship("HistorialProceso", backref='historialproceso', lazy=True)
 
   def save(self):
     db.session.add(self)
@@ -23,7 +24,8 @@ class Etapa(db.Model):
       'nombre': self.nombre,
       'etapafinal': self.etapafinal,
       'update': self.update,
-      'created': self.created
+      'created': self.created,
+      #'hsProcesos': [lo_hProceso.serialize() for lo_hProceso in self.hsProcesos]
     }
   
   def put(self):
