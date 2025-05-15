f3b4a720d8aa4bba9d24981742d228c82fda8661
santijulian16 <santijulian16@gmail.com>
2025-05-14 19:22:37 -0500

    Add AprobarOrdenHandler to manage order approval and update related pieces and processes

--- app/handlers/ordenes.py
+++ app/handlers/ordenes.py
@@ -1,6 +1,10 @@
 from flask_restful import Resource, request
 
 from app.models.orden import Orden
+from app.models.pieza import Pieza
+from app.models.proceso import Proceso
+from app.models.historial_proceso import HistorialProceso
+
 from time import time
 from flask_jwt_extended import jwt_required, get_jwt_identity
 
@@ -86,6 +90,56 @@ class OrderIDEstadoHandler(Resource):
         msg = {'message': 'Estado de Orden actualizado correctamente.'}
       else:
         msg = {'error': 'No existe la orden a actualizar.'}, 404
+    else:
+      msg = {'error': 'No existe atributo "ordenid" en payload enviado.'}, 404
+
+    return msg
+  
+class AprobarOrdenHandler(Resource):
+  @jwt_required()
+  def post(self):
+    data = request.json  # Datos enviados en la solicitud
+    msg = ""
+
+    if "ordenid" in data:
+      lo_orden = Orden.query.get(data["ordenid"])
+      if(lo_orden is not None):
+        # Busco usuario        
+        userid = get_jwt_identity()
+        
+        # Actualizado el estado de la orden a EN_PROCESO
+        lo_orden.estado = 'EN_PROCESO'
+        lo_orden.update = int(time())
+        l_today = int(time())        
+        lo_orden.put()
+        
+        # Busco y actualizo las piezas de la orden
+        piezas = Pieza.query.filter_by(ordenid=data["ordenid"]).all()
+        for lo_pieza in piezas:
+          # Actualizar solo si el dato es proporcionado
+          if "fechaentrega" in data:
+            lo_pieza.fechaentrega = data["fechaentrega"]
+          
+          lo_pieza.estado = 'EN_PROCESO'
+          lo_pieza.put()
+           
+          # Crear proceso de la pieza
+          lo_proceso = Proceso(estado='EN_PROCESO', fechainicial=l_today, idpieza=lo_pieza.idpieza, created=l_today, update=None)
+          lo_proceso.save()
+          
+          # Obetener la etapa de la pieza
+          etapas_rta = lo_pieza.rutaprdccion.rprdcion_etapas          
+          etapas_rta.sort(key=lambda lo_rprdcion_etapa: lo_rprdcion_etapa.orden)
+        
+          # Crear el historial de la pieza
+          lo_historial = HistorialProceso(estado='REGISTRADO', fechaingreso=l_today, idetapa=etapas_rta[0].idetapa, idproceso=lo_proceso.idproceso, idusuario=userid, created=l_today, update=None)
+          lo_historial.save()
+                      
+          msg = {'message': 'Orden aprobada para paso a producción correctamente.'}
+        
+        
+      else:
+        msg = {'error': 'No existe la orden a aprobar.'}, 404
     else:
       msg = {'error': 'No existe atributo "ordenid" en payload enviado.'}, 404
 
